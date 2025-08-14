3d00572121a79e7d362a38aa380cfc21bc179258
harshdabhi <harshdabhi67@gmail.com>
2025-08-14 08:44:07 +0000

    structure update

--- Code/sha_exec.sh
+++ Code/sha_exec.sh
@@ -2,39 +2,39 @@
 
 #<<<================================================================================================================================================================>>#
 
-echo "accelerated AES encryption test begins"
-/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -march=rv32imac_zbb_zbc -mabi=ilp32 -D USE_RISCV_ACCEL aes_dir/aes_filesv2/testv4.c -static -o aes_acc
-/usr/local/bin/qemu-riscv32 ./aes_acc
+echo "accelerated SHA encryption test begins"
+/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -march=rv32i_zbb_zbc_zbkb_zbkc_zbkx_zknd_zkne_zknh_zksed_zksh -mabi=ilp32 -D USE_RISCV_CRYPTO_EXT sha_dir/sha_filesv2/testv2.c -static -o sha_acc
+/usr/local/bin/qemu-riscv32 ./sha_acc
 
 #<<<================================================================================================================================================================>>#
 
-echo "Moving files from source to AES result directory"
-mv accelerated_results_aes.csv aes_result
-mv aes_acc aes_result
+echo "Moving files from source to SHA result directory"
+mv sha256_accelerated_results.csv sha_result
+mv sha_acc sha_result
+
 
 #<<<================================================================================================================================================================>>#
 
-echo "Removing files generated during test"
-rm temp_data.bin
-rm temp_data.enc
+# echo "Removing files generated during test"
+# rm temp_data.bin
+# rm temp_data.enc
 
 #<<<================================================================================================================================================================>>#
 
-echo "Standard AES encryption test begins"
-/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -march=rv32i -mabi=ilp32 aes_dir/aes_filesv2/testv4.c -static -o aes
-/usr/local/bin/qemu-riscv32 ./aes
+echo "Standard SHA encryption test begins"
+/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -march=rv32i -mabi=ilp32 sha_dir/sha_filesv2/testv2.c -static -o sha
+/usr/local/bin/qemu-riscv32 ./sha
 
 #<<<================================================================================================================================================================>>#
 
-echo "Removing files generated during test"
-rm temp_data.bin
-rm temp_data.enc
+# echo "Removing files generated during test"
+# rm temp_data.bin
+# rm temp_data.enc
 
 #<<<================================================================================================================================================================>>#
 
-echo "Moving files from source to AES result directory"
-mv standard_results_aes.csv aes_result
-mv aes aes_result
-
+echo "Moving files from source to SHA result directory"
+mv sha256_standard_results.csv sha_result
+mv sha sha_result
 
 #<<<================================================================================================================================================================>>#
